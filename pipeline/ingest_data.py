b93026595531ddd29795a2331848a1f1f1ab5f9b
Huong Nguyen <156730235+huongnguyen2024@users.noreply.github.com>
2026-01-23 23:41:09 +0000

    ingestion data and docker compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 # Specify Data types
 dtype = {
@@ -30,20 +31,21 @@ parse_dates = [
 
 
 # Ingest Data into Progres
-def run():  
-    # Parameters 
-    year = 2021
-    month = 1
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    chunksize=100000  
-    target_table = "yellow_taxi_data"
+@click.command()
+# @click.option('--year', default=2021, type=int, help='Year of data to ingest')
+# @click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+
+def run(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, chunksize, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
-
+    target_table = f'{target_table}_{year}_{month:02d}'
     # Create database connection
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
     
