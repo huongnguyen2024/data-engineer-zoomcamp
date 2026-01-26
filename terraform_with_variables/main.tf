20052531a0ecdf038907332af5cfb8f54eb77532
huongnguyen2024 <huongnguyen120595@gmail.com>
2026-01-26 12:51:48 -0800

    terraform intro workshop

---
+++ terraform_with_variables/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  #credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "data_lake_bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "my_bigquery_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
